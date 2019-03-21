c6a9cd9057c5327ec26dfa0a08806dddfdf7ae3c
Fernando Augusto Medeiros Silva <fernando.augusto@gmail.com>
2019-03-21 08:26:16 -0300

    meu contador simples

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+from redis import Redis
+
+from flask import Flask, session, render_template
+from flask_session import Session
+
+
+app = Flask(__name__, template_folder="templates")
+
+
+REDIS_HOST = os.environ.get("REDIS_HOST", default='localhost')
+REDIS_PORT = os.environ.get("REDIS_PORT", default=6379)
+SESSION_TYPE = 'redis'
+SESSION_REDIS = Redis(REDIS_HOST,port=REDIS_PORT)
+app.config.from_object(__name__)
+Session(app)
+
+
+
+
+@app.route("/")
+def index():
+    if 'contador' not in session:
+        session['contador'] = 1
+    else:
+        session['contador'] = int(session['contador']) + 1
+    contador = session['contador']
+    return render_template('index.html',acessos=contador)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
