55277bc3d8644dab7301ebd7d5398a6ebd5da16f
Fernando Augusto Medeiros Silva <fernando.augusto@gmail.com>
2019-05-28 10:54:51 -0300

    Adicionado hostname n

--- contador/app.py
+++ contador/app.py
@@ -8,15 +8,13 @@ from flask_session import Session
 app = Flask(__name__, template_folder="templates")
 versao = 0.1
 
+HOSTNAME = os.uname()[1]
 REDIS_HOST = os.environ.get("REDIS_HOST", default='localhost')
 REDIS_PORT = os.environ.get("REDIS_PORT", default=6379)
 SESSION_TYPE = 'redis'
 SESSION_REDIS = Redis(REDIS_HOST,port=REDIS_PORT)
 app.config.from_object(__name__)
 Session(app)
-
-
-
 
 @app.route("/")
 def index():
@@ -25,7 +23,7 @@ def index():
     else:
         session['contador'] = int(session['contador']) + 1
     contador = session['contador']
-    return render_template('index.html',acessos=contador , versao=versao )
+    return render_template('index.html',acessos=contador , versao=versao, hostname=HOSTNAME ) 
 
 
 if __name__ == "__main__":
